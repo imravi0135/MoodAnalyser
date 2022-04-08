b7507e2dd1540ff8e159796912682567f7ab1ee9
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-04-08 22:33:03 +0530

    Welcome to Mood Analyser testing Programs

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MoodAnalyserSpace
+{
+    public class MoodAnalyser
+    {
+        string Message;
+
+        public MoodAnalyser()
+        {
+        }
+
+        public MoodAnalyser(string Message)
+        {
+            this.Message = Message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (Message.ToLower().Contains("sad"))
+                return "SAD";
+            else return "HAPPY";
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
